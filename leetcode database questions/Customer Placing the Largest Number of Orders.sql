85f50318fb45bb67fe3c20de10ddb71e4bc967ed
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-21 17:39:14 +0530

    added solution

--- leetcode database questions/Customer Placing the Largest Number of Orders.sql
+++ leetcode database questions/Customer Placing the Largest Number of Orders.sql
@@ -1,49 +1,49 @@
-Table: Orders
+-- Table: Orders
 
-+-----------------+----------+
-| Column Name     | Type     |
-+-----------------+----------+
-| order_number    | int      |
-| customer_number | int      |
-+-----------------+----------+
-order_number is the primary key for this table.
-This table contains information about the order ID and the customer ID.
+-- +-----------------+----------+
+-- | Column Name     | Type     |
+-- +-----------------+----------+
+-- | order_number    | int      |
+-- | customer_number | int      |
+-- +-----------------+----------+
+-- order_number is the primary key for this table.
+-- This table contains information about the order ID and the customer ID.
  
 
-Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
+-- Write an SQL query to find the customer_number for the customer who has placed the largest number of orders.
 
-It is guaranteed that exactly one customer will have placed more orders than any other customer.
+-- It is guaranteed that exactly one customer will have placed more orders than any other customer.
 
-The query result format is in the following example:
+-- The query result format is in the following example:
 
  
 
-Orders table:
-+--------------+-----------------+
-| order_number | customer_number |
-+--------------+-----------------+
-| 1            | 1               |
-| 2            | 2               |
-| 3            | 3               |
-| 4            | 3               |
-+--------------+-----------------+
+-- Orders table:
+-- +--------------+-----------------+
+-- | order_number | customer_number |
+-- +--------------+-----------------+
+-- | 1            | 1               |
+-- | 2            | 2               |
+-- | 3            | 3               |
+-- | 4            | 3               |
+-- +--------------+-----------------+
 
-Result table:
-+-----------------+
-| customer_number |
-+-----------------+
-| 3               |
-+-----------------+
-The customer with number 3 has two orders, which is greater than either customer 1 or 2 because each of them only has one order. 
-So the result is customer_number 3.
+-- Result table:
+-- +-----------------+
+-- | customer_number |
+-- +-----------------+
+-- | 3               |
+-- +-----------------+
+-- The customer with number 3 has two orders, which is greater than either customer 1 or 2 because each of them only has one order. 
+-- So the result is customer_number 3.
  
 
-Follow up: What if more than one customer have the largest number of orders, can you find all the customer_number in this case?
+-- Follow up: What if more than one customer have the largest number of orders, can you find all the customer_number in this case?
 
 
 
 
-# first apporach 
+-- # first apporach 
 
 
 select customer_number from orders 
@@ -51,7 +51,8 @@ group by customer_number
 order by count(*) desc limit 1;
 
 
-# second approach 
+-- # second approach 
+
 select customer_number from orders 
 group by customer_number
 having count(order_number) >= all(select count(customer_number) from orders group by customer_number);
