1e2b18f9118bfaf29d1194aa6a73a20bf2c52bb1
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-21 18:53:48 +0530

    solution added

--- leetcode database questions/Biggest Single Number.sql
+++ leetcode database questions/Biggest Single Number.sql
@@ -21,3 +21,25 @@ For the sample data above, your query should return the following result:
 Note:
 If there is no such number, just output null.
  
+
+
+
+# first approach 
+
+select a.num from my_numbers a, my_numbers b
+where a.num = b.num group by a.num order by count(*) asc, a.num desc  limit 1;
+
+#second approach 
+
+select max(num) as num
+from(
+    select num from my_numbers group by num having count(num) = 1
+) as t;
+
+
+# third approach 
+select num from my_numbers group by num having count(num) = 1
+order by num desc limit 1;
+
+
+
