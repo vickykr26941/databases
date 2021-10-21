511fa9d2494212666322849f8d97418878267b9a
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-21 23:11:24 +0530

    solution added

--- leetcode database questions/Find the Team Size.sql
+++ leetcode database questions/Find the Team Size.sql
@@ -1,60 +1,60 @@
-Table: Employee
+-- Table: Employee
 
-+---------------+---------+
-| Column Name   | Type    |
-+---------------+---------+
-| employee_id   | int     |
-| team_id       | int     |
-+---------------+---------+
-employee_id is the primary key for this table.
-Each row of this table contains the ID of each employee and their respective team.
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | employee_id   | int     |
+-- | team_id       | int     |
+-- +---------------+---------+
+-- employee_id is the primary key for this table.
+-- Each row of this table contains the ID of each employee and their respective team.
  
 
-Write an SQL query to find the team size of each of the employees.
+-- Write an SQL query to find the team size of each of the employees.
 
-Return result table in any order.
+-- Return result table in any order.
 
-The query result format is in the following example.
+-- The query result format is in the following example.
 
  
 
-Example 1:
+-- Example 1:
 
-Input: 
-Employee Table:
-+-------------+------------+
-| employee_id | team_id    |
-+-------------+------------+
-|     1       |     8      |
-|     2       |     8      |
-|     3       |     8      |
-|     4       |     7      |
-|     5       |     9      |
-|     6       |     9      |
-+-------------+------------+
-Output: 
-+-------------+------------+
-| employee_id | team_size  |
-+-------------+------------+
-|     1       |     3      |
-|     2       |     3      |
-|     3       |     3      |
-|     4       |     1      |
-|     5       |     2      |
-|     6       |     2      |
-+-------------+------------+
-Explanation: 
-Employees with Id 1,2,3 are part of a team with team_id = 8.
-Employee with Id 4 is part of a team with team_id = 7.
-Employees with Id 5,6 are part of a team with team_id = 9.
+-- Input: 
+-- Employee Table:
+-- +-------------+------------+
+-- | employee_id | team_id    |
+-- +-------------+------------+
+-- |     1       |     8      |
+-- |     2       |     8      |
+-- |     3       |     8      |
+-- |     4       |     7      |
+-- |     5       |     9      |
+-- |     6       |     9      |
+-- +-------------+------------+
+-- Output: 
+-- +-------------+------------+
+-- | employee_id | team_size  |
+-- +-------------+------------+
+-- |     1       |     3      |
+-- |     2       |     3      |
+-- |     3       |     3      |
+-- |     4       |     1      |
+-- |     5       |     2      |
+-- |     6       |     2      |
+-- +-------------+------------+
+-- Explanation: 
+-- Employees with Id 1,2,3 are part of a team with team_id = 8.
+-- Employee with Id 4 is part of a team with team_id = 7.
+-- Employees with Id 5,6 are part of a team with team_id = 9.
 
 
-# first approach 
+-- # first approach 
 select employee_id, count(*) over(partition by team_id) as team_size
 from employee;
 
 
-# second approach 
+-- # second approach 
 select e1.employee_id,(
     select count(e2.team_id) from employee e2
     where e1.team_id = e2.team_id group by team_id
