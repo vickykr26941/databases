1f83c48a873618bac8a49b0faecc5706c65d4c21
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-22 00:21:15 +0530

    solution added

--- leetcode database questions/Article Views I.sql
+++ leetcode database questions/Article Views I.sql
@@ -1,57 +1,57 @@
-Table: Views
+-- Table: Views
 
-+---------------+---------+
-| Column Name   | Type    |
-+---------------+---------+
-| article_id    | int     |
-| author_id     | int     |
-| viewer_id     | int     |
-| view_date     | date    |
-+---------------+---------+
-There is no primary key for this table, it may have duplicate rows.
-Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
-Note that equal author_id and viewer_id indicate the same person.
+-- +---------------+---------+
+-- | Column Name   | Type    |
+-- +---------------+---------+
+-- | article_id    | int     |
+-- | author_id     | int     |
+-- | viewer_id     | int     |
+-- | view_date     | date    |
+-- +---------------+---------+
+-- There is no primary key for this table, it may have duplicate rows.
+-- Each row of this table indicates that some viewer viewed an article (written by some author) on some date. 
+-- Note that equal author_id and viewer_id indicate the same person.
  
 
-Write an SQL query to find all the authors that viewed at least one of their own articles.
+-- Write an SQL query to find all the authors that viewed at least one of their own articles.
 
-Return the result table sorted by id in ascending order.
+-- Return the result table sorted by id in ascending order.
 
-The query result format is in the following example.
+-- The query result format is in the following example.
 
  
 
-Example 1:
+-- Example 1:
 
-Input: 
-Views table:
-+------------+-----------+-----------+------------+
-| article_id | author_id | viewer_id | view_date  |
-+------------+-----------+-----------+------------+
-| 1          | 3         | 5         | 2019-08-01 |
-| 1          | 3         | 6         | 2019-08-02 |
-| 2          | 7         | 7         | 2019-08-01 |
-| 2          | 7         | 6         | 2019-08-02 |
-| 4          | 7         | 1         | 2019-07-22 |
-| 3          | 4         | 4         | 2019-07-21 |
-| 3          | 4         | 4         | 2019-07-21 |
-+------------+-----------+-----------+------------+
-Output: 
-+------+
-| id   |
-+------+
-| 4    |
-| 7    |
-+------+
+-- Input: 
+-- Views table:
+-- +------------+-----------+-----------+------------+
+-- | article_id | author_id | viewer_id | view_date  |
+-- +------------+-----------+-----------+------------+
+-- | 1          | 3         | 5         | 2019-08-01 |
+-- | 1          | 3         | 6         | 2019-08-02 |
+-- | 2          | 7         | 7         | 2019-08-01 |
+-- | 2          | 7         | 6         | 2019-08-02 |
+-- | 4          | 7         | 1         | 2019-07-22 |
+-- | 3          | 4         | 4         | 2019-07-21 |
+-- | 3          | 4         | 4         | 2019-07-21 |
+-- +------------+-----------+-----------+------------+
+-- Output: 
+-- +------+
+-- | id   |
+-- +------+
+-- | 4    |
+-- | 7    |
+-- +------+
 
 
 
-# first approach 
+-- # first approach 
 select author_id as id from `views` group by author_id,viewer_id
 having sum(author_id = viewer_id) > 0 order by id;
 
 
-# second approach 
+-- # second approach 
 select distinct author_id as id from `views` where author_id = viewer_id order by id;
 
 
