57f0c4c7dba6f9cf0a69ce83d0dbfd5e748deece
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-21 19:30:39 +0530

    Update and rename Friend Requests I: Overall Acceptance Rate to Friend Requests I: Overall Acceptance Rate.sql

--- leetcode database questions/Friend Requests I: Overall Acceptance Rate.sql
+++ leetcode database questions/Friend Requests I: Overall Acceptance Rate.sql
@@ -71,3 +71,17 @@ Could you write a query to return the acceptance rate for every month?
 Could you write a query to return the cumulative acceptance rate for every day?
 
 
+
+select 
+  round(
+     ifnull(
+       (select count(*) from (select distinct requester_id,accepter_id from RequestAccepted) as A)
+         /
+       (select count(*) from (select distinct sender_id,send_to_id from FriendRequest) as B)
+     ,0)
+  ,2) as accept_rate;
+  
+  
+  
+  
+  
