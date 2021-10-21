eeeab404d1694845e8423fb9af5951699003ce4e
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-21 18:54:22 +0530

    solution added

--- leetcode database questions/Biggest Single Number.sql
+++ leetcode database questions/Biggest Single Number.sql
@@ -1,35 +1,35 @@
-SQL Schema
-Table my_numbers contains many numbers in column num including duplicated ones.
-Can you write a SQL query to find the biggest number, which only appears once.
+-- SQL Schema
+-- Table my_numbers contains many numbers in column num including duplicated ones.
+-- Can you write a SQL query to find the biggest number, which only appears once.
 
-+---+
-|num|
-+---+
-| 8 |
-| 8 |
-| 3 |
-| 3 |
-| 1 |
-| 4 |
-| 5 |
-| 6 | 
-For the sample data above, your query should return the following result:
-+---+
-|num|
-+---+
-| 6 |
-Note:
-If there is no such number, just output null.
+-- +---+
+-- |num|
+-- +---+
+-- | 8 |
+-- | 8 |
+-- | 3 |
+-- | 3 |
+-- | 1 |
+-- | 4 |
+-- | 5 |
+-- | 6 | 
+-- For the sample data above, your query should return the following result:
+-- +---+
+-- |num|
+-- +---+
+-- | 6 |
+-- Note:
+-- If there is no such number, just output null.
  
 
 
 
-# first approach 
+-- # first approach 
 
 select a.num from my_numbers a, my_numbers b
 where a.num = b.num group by a.num order by count(*) asc, a.num desc  limit 1;
 
-#second approach 
+-- #second approach 
 
 select max(num) as num
 from(
@@ -37,7 +37,7 @@ from(
 ) as t;
 
 
-# third approach 
+-- # third approach 
 select num from my_numbers group by num having count(num) = 1
 order by num desc limit 1;
 
