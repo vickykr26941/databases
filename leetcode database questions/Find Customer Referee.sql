7865bfd81951457e7197eacd4a0e2a2a568dfcf6
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-21 17:18:01 +0530

    Update Find Customer Referee.sql

--- leetcode database questions/Find Customer Referee.sql
+++ leetcode database questions/Find Customer Referee.sql
@@ -1,30 +1,30 @@
-Given a table customer holding customers information and the referee.
+-- Given a table customer holding customers information and the referee.
 
-+------+------+-----------+
-| id   | name | referee_id|
-+------+------+-----------+
-|    1 | Will |      NULL |
-|    2 | Jane |      NULL |
-|    3 | Alex |         2 |
-|    4 | Bill |      NULL |
-|    5 | Zack |         1 |
-|    6 | Mark |         2 |
-+------+------+-----------+
-Write a query to return the list of customers NOT referred by the person with id '2'.
+-- +------+------+-----------+
+-- | id   | name | referee_id|
+-- +------+------+-----------+
+-- |    1 | Will |      NULL |
+-- |    2 | Jane |      NULL |
+-- |    3 | Alex |         2 |
+-- |    4 | Bill |      NULL |
+-- |    5 | Zack |         1 |
+-- |    6 | Mark |         2 |
+-- +------+------+-----------+
+-- Write a query to return the list of customers NOT referred by the person with id '2'.
 
-For the sample data above, the result is:
+-- For the sample data above, the result is:
 
-+------+
-| name |
-+------+
-| Will |
-| Jane |
-| Bill |
-| Zack |
-+------+
+-- +------+
+-- | name |
+-- +------+
+-- | Will |
+-- | Jane |
+-- | Bill |
+-- | Zack |
+-- +------+
 
-solution1
-select name from customer where referee_id != 2 or referee_id is null;
+-- solution1
+-- select name from customer where referee_id != 2 or referee_id is null;
 
-solution 2
+-- solution 2
 select name from customer where ifnull(referee_id,0) <> 2;
