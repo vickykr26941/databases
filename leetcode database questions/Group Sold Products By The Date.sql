75aa1436bc9c9e2a8dfb3cbb82ff519a24f09079
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-22 15:24:54 +0530

    Update and rename Group Sold Products By The Date to Group Sold Products By The Date.sql

--- leetcode database questions/Group Sold Products By The Date.sql
+++ leetcode database questions/Group Sold Products By The Date.sql
@@ -48,3 +48,6 @@ For 2020-05-30, Sold items were (Headphone, Basketball, T-shirt), we sort them l
 For 2020-06-01, Sold items were (Pencil, Bible), we sort them lexicographically and separate them by a comma.
 For 2020-06-02, the Sold item is (Mask), we just return it.
 
+
+select sell_date,count(distinct product) as num_sold,group_concat(distinct product order by product asc separator ',') as products from Activities
+group by sell_date order by sell_date;
