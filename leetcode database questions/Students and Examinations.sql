defa06e8372f68dbb8788ce044a65b28ce12d0f1
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-22 15:08:55 +0530

    Update and rename Students and Examinations to Students and Examinations.sql

--- leetcode database questions/Students and Examinations.sql
+++ leetcode database questions/Students and Examinations.sql
@@ -103,3 +103,12 @@ Alex did not attend any exams.
 John attended the Math exam 1 time, the Physics exam 1 time, and the Programming exam 1 time.
 
 
+
+
+select students.student_id,students.student_name,subjects.subject_name,count(examinations.subject_name) as attended_exams
+from students join subjects 
+left join examinations on students.student_id = examinations.student_id and subjects.subject_name = examinations.subject_name
+group by students.student_id,subjects.subject_name 
+order by student_id,subject_name;
+
+
