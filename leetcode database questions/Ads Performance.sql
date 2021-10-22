01f7541a2f783e4dcc001ce771e83a7ce2f642f6
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-22 13:57:43 +0530

    Update and rename Ads Performance to Ads Performance.sql

--- leetcode database questions/Ads Performance.sql
+++ leetcode database questions/Ads Performance.sql
@@ -61,3 +61,8 @@ Note that we do not care about Ignored Ads.
 
 
 
+select ad_id,round(ifnull(sum(`action` = 'Clicked') / (sum(`action` = 'Viewed') + sum(`action` = 'Clicked'))  * 100,0),2) as ctr from Ads
+group by ad_id order by ctr desc,ad_id asc;
+
+
+
