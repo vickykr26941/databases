adf23204ba018530f750adc9f6811b7478f99573
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-23 03:28:09 +0530

    Update and rename All Valid Triplets That Can Represent a Country to All Valid Triplets That Can Represent a Country.sql

--- leetcode database questions/All Valid Triplets That Can Represent a Country.sql
+++ leetcode database questions/All Valid Triplets That Can Represent a Country.sql
@@ -91,3 +91,10 @@ Let us see all the possible triplets.
 - (Bob, Tom, Jerry) --> Rejected because member_A and member_C have the same ID.
 - (Bob, Tom, Alice) --> Valid triplet.
 
+
+
+select a.student_name as member_A,b.student_name member_B,c.student_name member_C from SchoolA a,SchoolB b,SchoolC c
+where (a.student_id <> b.student_id and b.student_id <> c.student_id and a.student_id <> c.student_id) and
+      (a.student_name <> b.student_name and b.student_name <> c.student_name and a.student_name <> c.student_name);
+      
+      
