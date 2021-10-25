3f5d7d325f40bfab82d4612accfdaaaa2c509008
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-25 16:01:03 +0530

    Update and rename Primary Department for Each Employee to Primary Department for Each Employee.sql

--- leetcode database questions/Primary Department for Each Employee.sql
+++ leetcode database questions/Primary Department for Each Employee.sql
@@ -54,3 +54,7 @@ Explanation:
 - The Primary department for employee 4 is 3.
 
 
+select employee_id,department_id from employee where primary_flag = 'Y'
+union 
+(select employee_id,department_id from employee 
+group by employee_id having sum(primary_flag = 'N') = 1);
