cfccf73c879b2c15ff51ff5b0befce1b219f2a64
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-25 15:01:29 +0530

    solution added

--- leetcode database questions/Convert Date Format.sql
+++ leetcode database questions/Convert Date Format.sql
@@ -1,41 +1,41 @@
-Table: Days
+-- Table: Days
 
-+-------------+------+
-| Column Name | Type |
-+-------------+------+
-| day         | date |
-+-------------+------+
-day is the primary key for this table.
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | day         | date |
+-- +-------------+------+
+-- day is the primary key for this table.
  
 
-Write an SQL query to convert each date in Days into a string formatted as "day_name, month_name day, year".
+-- Write an SQL query to convert each date in Days into a string formatted as "day_name, month_name day, year".
 
-Return the result table in any order.
+-- Return the result table in any order.
 
-The query result format is in the following example.
+-- The query result format is in the following example.
 
  
 
-Example 1:
+-- Example 1:
 
-Input: 
-Days table:
-+------------+
-| day        |
-+------------+
-| 2022-04-12 |
-| 2021-08-09 |
-| 2020-06-26 |
-+------------+
-Output: 
-+-------------------------+
-| day                     |
-+-------------------------+
-| Tuesday, April 12, 2022 |
-| Monday, August 9, 2021  |
-| Friday, June 26, 2020   |
-+-------------------------+
-Explanation: Please note that the output is case-sensitive.
+-- Input: 
+-- Days table:
+-- +------------+
+-- | day        |
+-- +------------+
+-- | 2022-04-12 |
+-- | 2021-08-09 |
+-- | 2020-06-26 |
+-- +------------+
+-- Output: 
+-- +-------------------------+
+-- | day                     |
+-- +-------------------------+
+-- | Tuesday, April 12, 2022 |
+-- | Monday, August 9, 2021  |
+-- | Friday, June 26, 2020   |
+-- +-------------------------+
+-- Explanation: Please note that the output is case-sensitive.
 
 select date_format(day,"%W, %M %e, %Y") as day from days;
 
