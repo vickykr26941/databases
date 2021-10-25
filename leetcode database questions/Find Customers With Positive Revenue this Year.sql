49df341b4a53d617ee54c999711979ce1b4ca3be
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-25 15:05:42 +0530

    Update Find Customers With Positive Revenue this Year.sql

--- leetcode database questions/Find Customers With Positive Revenue this Year.sql
+++ leetcode database questions/Find Customers With Positive Revenue this Year.sql
@@ -1,53 +1,53 @@
-Table: Customers
+-- Table: Customers
 
-+--------------+------+
-| Column Name  | Type |
-+--------------+------+
-| customer_id  | int  |
-| year         | int  |
-| revenue      | int  |
-+--------------+------+
-(customer_id, year) is the primary key for this table.
-This table contains the customer ID and the revenue of customers in different years.
-Note that this revenue can be negative.
+-- +--------------+------+
+-- | Column Name  | Type |
+-- +--------------+------+
+-- | customer_id  | int  |
+-- | year         | int  |
+-- | revenue      | int  |
+-- +--------------+------+
+-- (customer_id, year) is the primary key for this table.
+-- This table contains the customer ID and the revenue of customers in different years.
+-- Note that this revenue can be negative.
  
 
-Write an SQL query to report the customers with postive revenue in the year 2021.
+-- Write an SQL query to report the customers with postive revenue in the year 2021.
 
-Return the result table in any order.
+-- Return the result table in any order.
 
-The query result format is in the following example.
+-- The query result format is in the following example.
 
  
 
-Example 1:
+-- Example 1:
 
-Input: 
-Customers table:
-+-------------+------+---------+
-| customer_id | year | revenue |
-+-------------+------+---------+
-| 1           | 2018 | 50      |
-| 1           | 2021 | 30      |
-| 1           | 2020 | 70      |
-| 2           | 2021 | -50     |
-| 3           | 2018 | 10      |
-| 3           | 2016 | 50      |
-| 4           | 2021 | 20      |
-+-------------+------+---------+
-Output: 
-+-------------+
-| customer_id |
-+-------------+
-| 1           |
-| 4           |
-+-------------+
-Explanation: 
-Customer 1 has revenue equal to 30 in the year 2021.
-Customer 2 has revenue equal to -50 in the year 2021.
-Customer 3 has no revenue in the year 2021.
-Customer 4 has revenue equal to 20 in the year 2021.
-Thus only customers 1 and 4 have positive revenue in the year 2021.
+-- Input: 
+-- Customers table:
+-- +-------------+------+---------+
+-- | customer_id | year | revenue |
+-- +-------------+------+---------+
+-- | 1           | 2018 | 50      |
+-- | 1           | 2021 | 30      |
+-- | 1           | 2020 | 70      |
+-- | 2           | 2021 | -50     |
+-- | 3           | 2018 | 10      |
+-- | 3           | 2016 | 50      |
+-- | 4           | 2021 | 20      |
+-- +-------------+------+---------+
+-- Output: 
+-- +-------------+
+-- | customer_id |
+-- +-------------+
+-- | 1           |
+-- | 4           |
+-- +-------------+
+-- Explanation: 
+-- Customer 1 has revenue equal to 30 in the year 2021.
+-- Customer 2 has revenue equal to -50 in the year 2021.
+-- Customer 3 has no revenue in the year 2021.
+-- Customer 4 has revenue equal to 20 in the year 2021.
+-- Thus only customers 1 and 4 have positive revenue in the year 2021.
 
 select customer_id from customers
 where `year` = '2021'
