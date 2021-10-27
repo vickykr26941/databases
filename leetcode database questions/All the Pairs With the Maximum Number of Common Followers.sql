c8891b3a740d5db8c414616b9aab54fbee359f41
vickykr26941 <67413576+vickykr26941@users.noreply.github.com>
2021-10-28 02:43:43 +0530

    Update and rename All the Pairs With the Maximum Number of Common Followers to All the Pairs With the Maximum Number of Common Followers.sql

--- leetcode database questions/All the Pairs With the Maximum Number of Common Followers.sql
+++ leetcode database questions/All the Pairs With the Maximum Number of Common Followers.sql
@@ -50,3 +50,23 @@ Users 2 and 7 have two common followers (3 and 4).
 Since the maximum number of common followers between any two users is 3, we return all pairs of users with three common followers, which is only the pair (1, 7). We return the pair as (1, 7), not as (7, 1).
 Note that we do not have any information about the users that follow users 3, 4, and 5, so we consider them to have 0 followers.
 
+
+
+
+
+select first_val user1_id,second_val user2_id from (
+    select a.user_id first_val,b.user_id second_val,count(a.follower_id) cnt from relations a,relations b
+    where a.follower_id = b.follower_id and a.user_id <> b.user_id and a.user_id < b.user_id
+    group by a.user_id,b.user_id
+    order by cnt desc
+) as t limit 1;
+
+
+-- using with 
+with tmp as (
+    select a.user_id x,b.user_id y,count(*) cnt from relations a, relations b
+    where a.follower_id = b.follower_id and a.user_id < b.user_id
+    group by a.user_id,b.user_id
+)
+select x user1_id,y user2_id from tmp
+where cnt = (select max(cnt) cnt from tmp);
